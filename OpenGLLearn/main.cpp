2082a00906411227d8ddcb78dfc007f49f740dff
zhaochunliang758 <zhaochunliang758@163.com>
2018-01-13 17:48:38 +0800

    引入 OpenGL 库，改造为一个 OpenGL 程序

--- OpenGLLearn/main.cpp
+++ OpenGLLearn/main.cpp
@@ -1,4 +1,9 @@
 #include <Windows.h>
+#include <gl/GL.h>
+
+#pragma comment(lib, "opengl32.lib");
+#pragma comment(lib, "glu32.lib");
+
 
 // define a self message process function
 LRESULT CALLBACK MyWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
@@ -39,6 +44,29 @@ INT WINAPI WinMain( __in HINSTANCE hInstance, __in_opt HINSTANCE hPrevInstance, 
 	// create the window
 	HWND hwnd = CreateWindowEx(NULL, L"GLWindow", L"OpenGLLearn", WS_OVERLAPPEDWINDOW, 100,100, 800, 600, NULL,NULL,hInstance,NULL);
 
+	// create OpenGL render content
+	HDC dc = GetDC(hwnd);
+	PIXELFORMATDESCRIPTOR pfd;
+	memset(&pfd, 0 , sizeof(PIXELFORMATDESCRIPTOR));
+	pfd.nVersion = 1;
+	pfd.nVersion = sizeof(PIXELFORMATDESCRIPTOR);
+	pfd.cColorBits = 32;
+	pfd.cDepthBits = 24;
+	pfd.cStencilBits = 8;
+	pfd.iPixelType   = PFD_TYPE_RGBA;
+	pfd.iLayerType   = PFD_MAIN_PLANE;
+	pfd.dwFlags      = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+
+	int pixelFormat = ChoosePixelFormat(dc, &pfd);
+	SetPixelFormat(dc,pixelFormat, &pfd);
+
+	// setup OpenGL context
+	HGLRC rc = wglCreateContext(dc);
+	wglMakeCurrent(dc, rc);
+
+	// init OpenGL
+	glClearColor(0.1, 0.4, 0.6, 1.0);
+
 	// show the window
 	ShowWindow(hwnd,SW_SHOW);
 	UpdateWindow(hwnd);
@@ -57,6 +85,13 @@ INT WINAPI WinMain( __in HINSTANCE hInstance, __in_opt HINSTANCE hPrevInstance, 
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		}
+
+		// draw scene
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
+		// present scene to window
+		SwapBuffers(dc);
 	}
 
 	return 0;
