7364e0b5ae60794f9a370f74cff2c25591ff05c3
zhaochunliang758 <zhaochunliang758@163.com>
2018-01-14 16:12:53 +0800

    用固定管线编程方式，画点、线、三角形、多边形等基本图元

--- OpenGLLearn/main.cpp
+++ OpenGLLearn/main.cpp
@@ -1,9 +1,14 @@
 #include <Windows.h>
 #include <gl/GL.h>
+#include <gl/GLU.h>
 
-#pragma comment(lib, "opengl32.lib");
-#pragma comment(lib, "glu32.lib");
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "glu32.lib")
 
+static unsigned int Pos_Left       = 10;
+static unsigned int Pos_Top        = 10;
+static unsigned int ViewPortWidth  = 1280;
+static unsigned int ViewPortHeight = 700;
 
 // define a self message process function
 LRESULT CALLBACK MyWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
@@ -16,6 +21,251 @@ LRESULT CALLBACK MyWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	}
 	return DefWindowProc(hwnd,msg,wParam,lParam);
 }
+
+void drawSence()
+{
+	// draw scene
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	// draw point
+	{
+		glPointSize(20.0f);
+		glDisable(GL_POINT_SMOOTH);
+		glDisable(GL_BLEND);
+		glBegin(GL_POINTS);
+			glColor4ub(255, 0, 0, 255);
+			glVertex3f(-1.0f, 4.0f, -10.0f);
+
+			glColor4ub(0, 255, 0, 255);
+			glVertex3f(0.0f, 4.0f, -10.0f);
+
+			glColor4ub(0, 0 ,255,255);
+			glVertex3f(1.0f, 4.0f, -10.0f);
+		glEnd();
+
+		glPointSize(15.0f);
+		glEnable(GL_POINT_SMOOTH);
+		glEnable(GL_BLEND);
+		glBegin(GL_POINTS);
+			glVertex3f(-2.0f, 4.0f, -10.0f);
+			glVertex3f(2.0f,  4.0f, -10.0f);
+		glEnd();
+		
+	}
+
+	// draw line
+	{
+		glLineWidth(3.0f);
+		glColor4ub(255, 0, 0, 255);
+		/*
+		GL_LINES: the line count = (int)(point count / 2)
+		*/
+		glBegin(GL_LINES);
+			// p0
+			glVertex3f(-6.0f, 2.5f, -10.0f);
+			// p1
+			glVertex3f(-3.0f, 2.5f, -10.0f);
+			// p2
+			glVertex3f(-6.0f, 1.5f, -10.0f);
+			// p3
+			glVertex3f(-3.0f, 1.5f, -10.0f);
+			// p4
+			glVertex3f(-3.0f, 1.0f, -10.0f);
+		glEnd();
+
+
+		glColor4ub(0, 255, 0, 255);
+		/*
+		GL_LINE_STRIP : line count == pt count -1; 
+		p0->p1; p1->p2; p2-p3
+		*/
+		glBegin(GL_LINE_STRIP);
+			// p0
+			glVertex3f(-2.5f, 2.5f, -10.0f);
+			// p1
+			glVertex3f(0.0f, 2.5f, -10.0f);
+			// p2
+			glVertex3f(-2.5f, 1.5f, -10.0f);
+			// p3
+			glVertex3f(0.0f, 1.5f, -10.0f);
+		glEnd();
+
+
+		glLineWidth(2.0f);
+		/*
+		GL_LINE_STRIP : line count == pt count -1; 
+		p0->p1; p1->p2; p2-p3
+		*/
+		glBegin(GL_LINE_STRIP);
+			// p0
+			glColor4ub(255, 255, 255, 255);
+			glVertex3f(0.5f, 2.5f, -10.0f);
+			// p1
+			glColor4ub(0, 255, 255, 255);
+			glVertex3f(2.5f, 2.5f, -10.0f);
+			// p3
+			glColor4ub(0, 0, 255, 255);
+			glVertex3f(0.5f, 1.5f, -10.0f);
+			// p4
+			glColor4ub(0, 0, 0, 255);
+			glVertex3f(2.5f, 1.5f, -10.0f);
+		glEnd();
+
+		glColor4ub(0, 0, 255, 255);
+		/*
+		GL_LINE_LOOP : line count == pt count; 
+		p0->p1; p1->p2; p2-p3, p3->p0
+		*/
+		glBegin(GL_LINE_LOOP);
+			// p0
+			glVertex3f(3.0f, 2.5f, -10.0f);
+			// p1
+			glVertex3f(5.0f, 2.5f, -10.0f);
+			// p2
+			glVertex3f(3.0f, 1.5f, -10.0f);
+			// p3
+			glVertex3f(5.0f, 1.5f, -10.0f);
+		glEnd();
+	}
+
+	// draw triangle
+	{
+		/*
+		GL_TRIANGLES : triangle count == (int)(pt count / 3); 
+		p0->p1->p2;
+		*/
+		glBegin(GL_TRIANGLES);
+			// p0
+			glVertex3f(-5.0f, -1.0f + 0.5f, -10.f);
+			// p1
+			glVertex3f(-3.0f, -1.0f + 0.5f, -10.f);
+			// p2
+			glVertex3f(-5.0f, -2.5f + 0.5f, -10.f);
+		glEnd();
+
+		/*
+		GL_TRIANGLE_STRIP : triangle count ; 
+		p0->p1->p2;
+		*/
+		glBegin(GL_TRIANGLE_STRIP);
+			// p0
+			glColor4ub(255, 0, 0, 255);
+			glVertex3f(-2.5f, -1.0f + 0.5f, -10.f);
+			// p1
+			glColor4ub(0, 255, 0, 255);
+			glVertex3f(-0.5f, -1.0f + 0.5f, -10.f);
+			// p2
+			glColor4ub(0, 0, 255, 255);
+			glVertex3f(-2.5f, -2.5f + 0.5f, -10.f);
+		glEnd();
+
+		/*
+		GL_TRIANGLE_STRIP : triangle count ; 
+		p0->p1->p2; p2->p1->p3;
+		*/
+		glBegin(GL_TRIANGLE_STRIP);
+			// p0
+			glColor4ub(255, 0, 0, 255);
+			glVertex3f(0.0f, -1.0f + 0.5f, -10.f);
+			// p1
+			glColor4ub(0, 255, 0, 255);
+			glVertex3f(2.0f, -1.0f + 0.5f, -10.f);
+			// p2
+			glColor4ub(0, 0, 255, 255);
+			glVertex3f(0.0f, -2.5f + 0.5f, -10.f);
+			// p3
+			glColor4ub(255, 255, 255, 255);
+			glVertex3f(2.0f, -2.5f + 0.5f, -10.f);
+		glEnd();
+
+		/*
+		GL_TRIANGLE_FAN : triangle count ; 
+		p0->p1->p2; p0->p2->p3; p0->p3->p4; p0->p4->p5;
+		*/
+		glBegin(GL_TRIANGLE_FAN);
+			// p0
+			glColor4ub(255, 255, 255, 255);
+			glVertex3f(3.5f, -2.0f + 0.5f, -10.f);
+			// p1
+			glColor4ub(0, 0, 0, 255);
+			glVertex3f(3.5f, -3.0f + 0.5f, -10.f);
+			// p2
+			glColor4ub(255, 0, 0, 255);
+			glVertex3f(4.5f, -2.0f + 0.5f, -10.f);
+			// p3
+			glColor4ub(0, 255, 0, 255);
+			glVertex3f(3.5f, -1.0f + 0.5f, -10.f);
+			// p4
+			glColor4ub(0, 0, 255, 255);
+			glVertex3f(2.5f, -2.0f + 0.5f, -10.f);
+			// p5
+			glColor4ub(0, 0, 0, 255);
+			glVertex3f(3.5f, -3.0f + 0.5f, -10.f);
+		glEnd();
+	}
+
+	// draw QUAD
+	{
+		glColor4ub(0, 0, 255, 255);
+		glBegin(GL_QUADS);
+			glVertex3f(-5.0f, -3.0f, -10.0f);
+			glVertex3f(-5.0f, -4.0f, -10.0f);
+			glVertex3f(-3.0f, -4.0f, -10.0f);
+			glVertex3f(-3.0f, -3.0f, -10.0f);
+		glEnd();
+
+		glColor4ub(0, 255, 0, 255);
+		glBegin(GL_QUAD_STRIP);
+			glVertex3f(-2.5f, -3.0f, -10.0f);
+			glVertex3f(-2.5f, -4.0f, -10.0f);
+			glVertex3f(-1.0f, -4.0f, -10.0f);
+			glVertex3f(-1.0f, -3.0f, -10.0f);
+		glEnd();
+
+	}
+
+	// draw polygon
+	{
+		glColor4ub(255, 0, 0, 255);
+		glBegin(GL_POLYGON);
+			glVertex3f(-0.5f, -3.0f, -10.0f);
+			glVertex3f(-0.5f, -4.0f, -10.0f);
+			glVertex3f(1.5f, -4.0f, -10.0f);
+			glVertex3f(1.5f, -3.0f, -10.0f);
+		glEnd();
+
+		glColor4ub(255, 255, 0, 255);
+		glPolygonMode(GL_FRONT,GL_POINT);
+		glPointSize(5.0f);
+		glBegin(GL_POLYGON);
+			glVertex3f(2.0f, -3.0f, -10.0f);
+			glVertex3f(2.0f, -4.0f, -10.0f);
+			glVertex3f(2.5f, -4.0f, -10.0f);
+			glVertex3f(2.5f, -3.0f, -10.0f);
+		glEnd();
+
+		glColor4ub(0, 255, 255, 255);
+		glPolygonMode(GL_FRONT,GL_LINE);
+		glLineWidth(5.0f);
+		glBegin(GL_POLYGON);
+			glVertex3f(3.0f, -3.0f, -10.0f);
+			glVertex3f(3.0f, -4.0f, -10.0f);
+			glVertex3f(3.5f, -4.0f, -10.0f);
+			glVertex3f(3.5f, -3.0f, -10.0f);
+
+		glEnd();
+
+		glColor4ub(255, 0, 255, 255);
+		glPolygonMode(GL_FRONT,GL_FILL);
+		glBegin(GL_POLYGON);
+			glVertex3f(4.0f, -3.0f, -10.0f);
+			glVertex3f(4.0f, -4.0f, -10.0f);
+			glVertex3f(4.5f, -4.0f, -10.0f);
+			glVertex3f(4.5f, -3.0f, -10.0f);
+		glEnd();
+	}
+}
+
 
 INT WINAPI WinMain( __in HINSTANCE hInstance, __in_opt HINSTANCE hPrevInstance, __in_opt LPSTR lpCmdLine, __in int nShowCmd )
 {
@@ -42,7 +292,7 @@ INT WINAPI WinMain( __in HINSTANCE hInstance, __in_opt HINSTANCE hPrevInstance, 
 	}
 
 	// create the window
-	HWND hwnd = CreateWindowEx(NULL, L"GLWindow", L"OpenGLLearn", WS_OVERLAPPEDWINDOW, 100,100, 800, 600, NULL,NULL,hInstance,NULL);
+	HWND hwnd = CreateWindowEx(NULL, L"GLWindow", L"OpenGLLearn", WS_OVERLAPPEDWINDOW, Pos_Left,Pos_Top, ViewPortWidth, ViewPortHeight, NULL,NULL,hInstance,NULL);
 
 	// create OpenGL render content
 	HDC dc = GetDC(hwnd);
@@ -65,7 +315,16 @@ INT WINAPI WinMain( __in HINSTANCE hInstance, __in_opt HINSTANCE hPrevInstance, 
 	wglMakeCurrent(dc, rc);
 
 	// init OpenGL
-	glClearColor(0.1, 0.4, 0.6, 1.0);
+	glMatrixMode(GL_PROJECTION);
+	GLdouble fovy   = 50.0f;                        //eye opened angle
+	GLdouble aspect = ViewPortWidth/ViewPortHeight; //width / height
+	GLdouble zNear  = 0.1f, zFar = 1000.0f;         //the neareast and farest distance which eye can see;
+	gluPerspective(fovy, aspect, zNear, zFar);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+
+	glClearColor(0.1f, 0.4f, 0.6f, 1.0f);
 
 	// show the window
 	ShowWindow(hwnd,SW_SHOW);
@@ -86,9 +345,7 @@ INT WINAPI WinMain( __in HINSTANCE hInstance, __in_opt HINSTANCE hPrevInstance, 
 			DispatchMessage(&msg);
 		}
 
-		// draw scene
-		glClear(GL_COLOR_BUFFER_BIT);
-
+		drawSence();
 
 		// present scene to window
 		SwapBuffers(dc);
